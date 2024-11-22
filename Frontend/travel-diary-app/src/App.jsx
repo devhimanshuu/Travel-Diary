676a090d0c7688eb346a0c5bf5688f9f7856c3f2
devhimanshuu <devhimanshuu@gmail.com>
2024-11-23 01:56:10 +0530

    Add root component for initial redirection based on authentication status

--- Frontend/travel-diary-app/src/App.jsx
+++ Frontend/travel-diary-app/src/App.jsx
@@ -12,12 +12,27 @@ const App = () => {
     <div>
       <Router>
         <Routes>
+          <Route path="/" exact element={<Root />} />
           <Route path="/dashboard" exact element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
         </Routes>
       </Router>
     </div>
+  );
+};
+
+//Define the root component to handle the initial redirection
+
+const Root = () => {
+  //check if the token is present in the local storage
+  const isAuthenticted = !!localStorage.getItem("token");
+
+  //redirect to the dashboard if the user is authenticated otherwise redirect to the login page
+  return isAuthenticted ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
   );
 };
 
