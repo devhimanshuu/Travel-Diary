9c5f24766fd435263ba2f9614675ccedf28b2d98
devhimanshuu <devhimanshuu@gmail.com>
2024-11-24 01:32:09 +0530

    Add function to generate initials from a name

--- Frontend/travel-diary-app/src/utils/helper.js
+++ Frontend/travel-diary-app/src/utils/helper.js
@@ -2,3 +2,15 @@ export const validateEmail = (email) => {
   const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regexEmail.test(email);
 };
+
+export const getIntials = (name) => {
+  if (!name) return "";
+
+  const words = name.split(" ");
+  let initials = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
