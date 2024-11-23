5170508b748fc924c2cac32ae2fe9555a2ca49c1
devhimanshuu <devhimanshuu@gmail.com>
2024-11-24 01:32:02 +0530

    Fetch user information and integrate Navbar component in Home page

--- Frontend/travel-diary-app/src/pages/Home/Home.jsx
+++ Frontend/travel-diary-app/src/pages/Home/Home.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import axiosInstance from "../../utils/axiosInstance";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [userInfo, setUserInfo] = useState(null);
+  const navigate = useNavigate();
+
+  //get user info
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (!response.data && response.data.user) {
+        //set the user info if data is present
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        //redirect to login page if the token is invalid or unauthorized
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <Navbar userInfo={userInfo} />
+    </>
+  );
 };
 
 export default Home;
